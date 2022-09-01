988c0a97a286107c92e720eaaf82dffe6c65a537
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-09-02 06:49:30 +0800

    updated the frameless window prototype

--- _prototypes/custom titlebar with menu, toolbar and statusbar/menubar_on_titlebar_frameless_window.py
+++ _prototypes/custom titlebar with menu, toolbar and statusbar/menubar_on_titlebar_frameless_window.py
@@ -5,6 +5,14 @@ from PyQt5.QtWidgets import (QMainWindow, QApplication, QToolButton, QHBoxLayout
                              QLabel, QSizeGrip, QMenuBar, QStyleFactory, qApp, QSizePolicy)
 from PyQt5.QtGui import QIcon, QPalette, QColor, QCursor
 
+import sys
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui as qtg
+from PyQt5 import QtPrintSupport
+
+import resources
+
 # TITLEBAR + MENU:                  https://pyquestions.com/pyqt-how-to-create-custom-combined-titlebar-and-menubar
 # CUSTOM TITLEBAR1:                 https://stackoverflow.com/questions/44241612/custom-titlebar-with-frame-in-pyqt5
 # CUSTOM TITLEBAR2:                 https://stackoverflow.com/questions/9377914/how-to-customize-title-bar-and-window-of-desktop-application
@@ -28,7 +36,7 @@ class TitleBar(QWidget):
     def __init__(self, parent):
         super(TitleBar, self).__init__()
         
-        qss = """
+        css = """
             QMenuBar{
                 color: #fff;
                 font: "Consolas";
@@ -40,7 +48,7 @@ class TitleBar(QWidget):
                 background-color: #242526; 
                 font-size: 13px;
                 font: "Consolas";
-                padding-right: 275px;
+                padding-right: 425px;
             }
             QToolButton[accessibleName="btn_close"] {
                 image: url(./icons/close_def.png);
@@ -67,7 +75,7 @@ class TitleBar(QWidget):
             }
 
 
-            """
+        """
         self.css_maximize ="""
             QToolButton[accessibleName="btn_max"] {
                 image: url(./icons/maximize_def.png);
@@ -96,34 +104,89 @@ class TitleBar(QWidget):
                 
             }
         """
+        self._createActions()
         
-        main = parent
         self.layout = QHBoxLayout()
         self.layout.setContentsMargins(0,0,10,0)
-        self.menu_bar = QMenuBar()
-        self.menu_bar.setStyleSheet(qss)  
-        self.menu_file = self.menu_bar.addMenu('File')
-        self.menu_file_quit=self.menu_file.addAction('Exit')
-        self.menu_file_quit.triggered.connect(qApp.quit)
+        self.menubar = QMenuBar()
+        self.menubar.setStyleSheet(css)  
+        
+        file_menu = self.menubar.addMenu('File')
+        file_menu.addAction(self.new_action)
+        file_menu.addAction(self.open_action)
+        file_menu.addAction(self.save_action)
+        file_menu.addSeparator()
+        file_menu.addAction(self.export_as_odt_action)
+        file_menu.addAction(self.export_as_pdf_action)
+        file_menu.addSeparator()
+        file_menu.addAction(self.print_action)
+        file_menu.addAction(self.preview_action)
+        file_menu.addSeparator()
+        file_menu.addAction(self.exit_action)
 
-        self.menu_edit=self.menu_bar.addMenu('Edit')
-        self.menu_help=self.menu_bar.addMenu('Help')
+        edit_menu= self.menubar.addMenu('Edit')
+        edit_menu.addAction(self.select_all_action)
+        edit_menu.addSeparator()
+        edit_menu.addAction(self.cut_action)
+        edit_menu.addAction(self.copy_action)
+        edit_menu.addAction(self.paste_action)
+        edit_menu.addSeparator()
+        edit_menu.addAction(self.undo_action)
+        edit_menu.addAction(self.redo_action)
+        
+        format_menu = self.menubar.addMenu("Format")
+        format_menu.addAction(self.strike_out_text_action)
+        format_menu.addAction(self.bold_text_action)
+        format_menu.addAction(self.italic_text_action)
+        format_menu.addAction(self.underline_text_action)
+        format_menu.addSeparator()
+        format_menu.addAction(self.superscript_text_action)
+        format_menu.addAction(self.subscript_text_action)
+        format_menu.addSeparator()
+        format_menu.addAction(self.number_list_action)
+        format_menu.addAction(self.bullet_list_action)
+        format_menu.addSeparator()
+        format_menu.addAction(self.align_left_action)
+        format_menu.addAction(self.align_center_action)
+        format_menu.addAction(self.align_right_action)
+        format_menu.addAction(self.align_justify_action)
+        format_menu.addAction(self.indent_action)
+        format_menu.addAction(self.unindent_action)
+        format_menu.addSeparator()
 
-        self.layout.addWidget(self.menu_bar) 
+        # color for toolbar
+        pix = qtg.QPixmap(20, 20)
+        pix.fill(qtc.Qt.black) 
+        self.text_color_action = qtw.QAction(qtg.QIcon(pix), "Colors", self,
+                triggered=self.textColor)
+        self.text_color_action.setShortcut("Ctrl+Shift+C")
+        self.text_color_action.setStatusTip("Allows users to pick a color of their choice")
+        format_menu.addAction(self.text_color_action)
+        format_menu.addAction(self.font_dialog_action)
+
+        insert_menu = self.menubar.addMenu("Insert")
+        insert_menu.addAction(self.insert_image_action) 
+
+        view_menu = self.menubar.addMenu("View")
+        view_menu.addAction(self.fullscreen_action) 
+        view_menu.addSeparator()
+        view_menu.addAction(self.view_status_action)
+        
+        self.layout.addWidget(self.menubar) 
 
         self.window_title = QLabel("Visual Studio Code")
         self.window_title.setAlignment(Qt.AlignCenter)
         self.window_title.setAccessibleName("lbl_title") 
         self.window_title.setFixedHeight(self.height)
         self.layout.addWidget(self.window_title)
-        self.window_title.setStyleSheet(qss)
+        self.window_title.setStyleSheet(css)
 
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         self.maxNormal=False
        
         self.closeButton = QToolButton() 
         self.closeButton.setAccessibleName("btn_close")                           
-        self.closeButton.setStyleSheet(qss)
+        self.closeButton.setStyleSheet(css)
         self.closeButton.clicked.connect(self.on_click_close)
 
         self.maxButton = QToolButton()
@@ -134,7 +197,7 @@ class TitleBar(QWidget):
         self.hideButton = QToolButton()
         self.hideButton.setAccessibleName("btn_min")  
         self.hideButton.clicked.connect(self.on_click_hide)
-        self.hideButton.setStyleSheet(qss)
+        self.hideButton.setStyleSheet(css)
 
         self.layout.addWidget(self.hideButton)
         self.layout.addWidget(self.maxButton)
@@ -145,17 +208,150 @@ class TitleBar(QWidget):
         self.pressing = False
         self.maxNormal=False
 
-        css= """
+        css_invisible_gripSize = """
                 background-color: transparent; 
         """
         # resize frame top left
         self.gripSize = 16
         self.grip_topLeft = QSizeGrip(self)
         self.grip_topLeft.resize(self.gripSize, self.gripSize)
-        self.grip_topLeft.setStyleSheet(css)
+        self.grip_topLeft.setStyleSheet(css_invisible_gripSize)
         self.grip_topRight = QSizeGrip(self)
         self.grip_topRight.resize(self.gripSize, self.gripSize)
-        self.grip_topRight.setStyleSheet(css)
+        self.grip_topRight.setStyleSheet(css_invisible_gripSize)
+
+    def _createActions(self):
+        # FILE MENU
+        self.new_action = qtw.QAction(qtg.QIcon(":/images/new_file.png"),"New", self)
+        self.open_action = qtw.QAction(qtg.QIcon(":/images/folder.png"),"Open", self)
+        self.save_action = qtw.QAction(qtg.QIcon(":/images/save.png"),"Save", self)
+        self.exit_action = qtw.QAction(qtg.QIcon(":/images/close.png"), "Exit", self)
+        self.export_as_odt_action = qtw.QAction(qtg.QIcon(":/images/odt.png"), "Export as OpenOffice Document", self)
+        self.export_as_pdf_action = qtw.QAction(qtg.QIcon(":/images/pdf.png"), "Export as PDF Document", self)
+        self.print_action = qtw.QAction(qtg.QIcon(":/images/print.png"), "Print Document", self)
+        self.preview_action = qtw.QAction(qtg.QIcon(":/images/preview.png"), "Page View", self)
+
+        self.new_action.setShortcut("Ctrl+N")
+        self.open_action.setShortcut("Ctrl+O")
+        self.save_action.setShortcut("Ctrl+S")
+        self.exit_action.setShortcut("Ctrl+Shift+Q")
+        self.export_as_odt_action.setShortcut("Alt+O")
+        self.export_as_pdf_action.setShortcut("Alt+P")
+        self.print_action.setShortcut("Ctrl+P")
+        self.preview_action.setShortcut("Ctrl+Shift+P")
+
+        self.new_action.setStatusTip("New file")
+        self.open_action.setStatusTip("Open a file")
+        self.save_action.setStatusTip("Save a file")
+        self.exit_action.setStatusTip("Exit Program")
+        self.export_as_odt_action.setStatusTip("Export your file as an OpenOffice document")
+        self.export_as_pdf_action.setStatusTip("Export your file as PDF document")
+        self.print_action.setStatusTip("Print document")
+        self.preview_action.setStatusTip("Preview page before printing")
+
+        # EDIT MENU
+        self.select_all_action = qtw.QAction(qtg.QIcon(":/images/select_all.png"), "Select All", self)
+        self.cut_action = qtw.QAction(qtg.QIcon(":/images/cut.png"), "Cut", self)
+        self.copy_action = qtw.QAction(qtg.QIcon(":/images/copy.png"), "Copy", self)
+        self.paste_action = qtw.QAction(qtg.QIcon(":/images/paste.png"), "Paste", self)
+        self.undo_action = qtw.QAction(qtg.QIcon(":/images/undo.png"), "Undo", self)
+        self.redo_action = qtw.QAction(qtg.QIcon(":/images/redo.png"), "Redo", self)
+        
+        self.select_all_action.setShortcut("Ctrl+A")
+        self.cut_action.setShortcut("Ctrl+X")
+        self.copy_action.setShortcut("Ctrl+C")
+        self.paste_action.setShortcut("Ctrl+V")
+        self.undo_action.setShortcut("Ctrl+Z")
+        self.redo_action.setShortcut("Ctrl+Y")
+
+        self.select_all_action.setStatusTip("Selects all texts")
+        self.cut_action.setStatusTip("Cuts the selected text and copies it to the clipboard")
+        self.copy_action.setStatusTip("Copies the selected text to the clipboard")
+        self.paste_action.setStatusTip("Pastes the clipboard text into the text editor")
+        self.undo_action.setStatusTip("Undo the previous operation")
+        self.redo_action.setStatusTip("Redo the previous operation")
+
+        # MISC MENU
+        self.insert_image_action = qtw.QAction(qtg.QIcon(":/images/insert_image.png"),"Insert image",self)
+        self.insert_image_action.setStatusTip("Insert image")
+        self.insert_image_action.setShortcut("Ctrl+Shift+I")
+        
+        # FORMAT MENU
+        self.bold_text_action = qtw.QAction(qtg.QIcon(":/images/bold.png"), "Bold", self)
+        self.italic_text_action = qtw.QAction(qtg.QIcon(":/images/italic.png"), "Italic", self)
+        self.underline_text_action = qtw.QAction(qtg.QIcon(":/images/underline.png"), "Underline", self)
+        self.strike_out_text_action = qtw.QAction(qtg.QIcon(":/images/strikeout.png"), "Strikeout", self)
+        self.superscript_text_action = qtw.QAction(qtg.QIcon(":/images/superscript.png"), "Superscript", self)
+        self.subscript_text_action = qtw.QAction(qtg.QIcon(":/images/subscript.png"), "Subscript", self)
+        self.align_left_action = qtw.QAction(qtg.QIcon(":/images/left_align.png"), "Align Left", self)
+        self.align_right_action = qtw.QAction(qtg.QIcon(":/images/right_align.png"), "Align Right", self)
+        self.align_center_action = qtw.QAction(qtg.QIcon(":/images/center_align.png"), "Align Center", self)
+        self.align_justify_action = qtw.QAction(qtg.QIcon(":/images/justify.png"), "Align Justify", self)
+        self.indent_action = qtw.QAction(qtg.QIcon(":/images/indent.png"), "Indent", self)
+        self.unindent_action = qtw.QAction(qtg.QIcon(":/images/unindent.png"), "Unindent", self)
+
+        self.color_action = qtw.QAction(qtg.QIcon(":/images/colour.png"), "Colors", self)
+        self.font_dialog_action = qtw.QAction(qtg.QIcon(":/images/text.png"), "Default Font", self)
+        self.number_list_action = qtw.QAction(qtg.QIcon(":/images/number_list.png"), "Numbering", self)
+        self.bullet_list_action = qtw.QAction(qtg.QIcon(":/images/bullet_list.png"), "Bullets", self)
+
+        # self.zoom_in_action = qtw.QAction(qtg.QIcon(":/images/zoom_in.png"), "Zoom In", self)
+        # self.zoom_out_action = qtw.QAction(qtg.QIcon(":/images/zoom_out.png"), "Zoom Out", self)
+        # self.zoom_default_action = qtw.QAction(qtg.QIcon(":/images/reset.png"), "Restore", self)
+
+        self.bold_text_action.setShortcut("Ctrl+B")
+        self.italic_text_action.setShortcut("Ctrl+I")
+        self.underline_text_action.setShortcut("Ctrl+U")
+        self.strike_out_text_action.setShortcut("Ctrl+/")
+        self.superscript_text_action.setShortcut("") # for some reason, superscript shortcut does not work 
+        self.subscript_text_action.setShortcut("")  # for some reason, subscript shortcut does not work
+        self.align_left_action.setShortcut("Ctrl+L")
+        self.align_right_action.setShortcut("Ctrl+R")
+        self.align_center_action.setShortcut("Ctrl+E")
+        self.align_justify_action.setShortcut("Ctrl+J")
+        self.font_dialog_action.setShortcut("Ctrl+Shift+F")
+        self.number_list_action.setShortcut("Alt+1")
+        self.bullet_list_action.setShortcut("Alt+.")
+        self.indent_action.setShortcut("Ctrl+Tab")
+        self.unindent_action.setShortcut("Shift+Tab")
+        # self.zoom_in_action.setShortcut("Ctrl+=") 
+        # self.zoom_out_action.setShortcut("Ctrl+-") 
+        # self.zoom_default_action.setShortcut("Ctrl+0")
+ 
+        self.bold_text_action.setStatusTip("Toggle whether the font weight is bold or not")
+        self.italic_text_action.setStatusTip("Toggle whether the font is italic or not")
+        self.underline_text_action.setStatusTip("Toggle whether the font is underlined or not")
+        self.strike_out_text_action.setStatusTip("Toggle whether the font is striked out or not")
+        self.superscript_text_action.setShortcut("Type very small letters just above the line of text")
+        self.subscript_text_action.setShortcut("Type very small letters just below the line of text")
+        self.align_left_action.setStatusTip("Aligns with the left edge")
+        self.align_right_action.setStatusTip("Aligns with the right edge")
+        self.align_center_action.setStatusTip("Centers horizontally in the available space")
+        self.align_justify_action.setStatusTip("Justifies the text in the available space")
+        self.color_action.setStatusTip("Pick a color of their choice")
+        self.font_dialog_action.setStatusTip("Set a font for all texts")
+        self.number_list_action.setStatusTip("Create numbered list")
+        self.bullet_list_action.setStatusTip("Create bulleted list")
+        self.indent_action.setStatusTip("Indent selection")
+        self.unindent_action.setStatusTip("Unindent selection")
+        # self.zoom_in_action.setStatusTip("Zoom In") 
+        # self.zoom_out_action.setStatusTip("Zoom Out") 
+        # self.zoom_default_action.setStatusTip("Restore to the default font size")
+
+        # VIEW MENU
+        self.fullscreen_action = qtw.QAction(qtg.QIcon(":/images/fullscreen.png"), "Fullscreen", self)
+        self.view_status_action = qtw.QAction('Show Statusbar', self, checkable=True)
+        
+        self.fullscreen_action.setShortcut("F11")
+        self.view_status_action.setShortcut("")
+
+        self.fullscreen_action.setStatusTip("Toggles the full screen mode")
+        self.view_status_action.setStatusTip('Toggle the status bar to be visible or not')
+        self.view_status_action.setChecked(True)
+
+    # toolbar update display color depending on color selected
+    def textColor(self):
+        pass
 
     def resizeEvent(self, event):
         QMainWindow.resizeEvent(self, event)
@@ -290,9 +486,6 @@ class StatusBar(QWidget):
     def showMessage(self, text):
         self.label.setText(text)
 
-
-   
-      
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -467,3 +660,4 @@ if __name__ == "__main__":
     main = MainWindow()
     main.show()
     sys.exit(app.exec_())
+
