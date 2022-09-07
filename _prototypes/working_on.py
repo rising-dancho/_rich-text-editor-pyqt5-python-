fd6be61af66fcaa8168e0e3fcfbe08013b8e0f35
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-09-07 20:12:33 +0800

    cleaned the css codes

--- _prototypes/working_on.py
+++ _prototypes/working_on.py
@@ -18,88 +18,14 @@ class TitleBar(qtw.QWidget):
     def __init__(self, parent):
         super(TitleBar, self).__init__()
         
-        css = """
-            QMenuBar{
-                color: #fff;
-                font: "Consolas";
-                font-size: 14px;
-                padding: 3px; 
-            }
-           
-            QLabel[accessibleName="lbl_title"]{
-                background-color: #161a21; 
-                font-size: 13px;
-                font: "Consolas";
-                padding-right: 425px;
-            }
-            QToolButton[accessibleName="btn_close"] {
-                image: url(./icons/nav_close.png);
-                background: #161a21;
-                border: none;
-               
-            }
-            QToolButton[accessibleName="btn_close"]:hover {
-                image: url(./icons/colored_close.png);
-                background: #161a21;
-                border: none;
-            }    
-            QToolButton[accessibleName="btn_min"] {
-                image: url(./icons/nav_minimize.png);
-                background: #161a21;
-                border: none;
-                padding-right: 3px;
-            }
-            QToolButton[accessibleName="btn_min"]:hover {
-                image: url(./icons/colored_minimize.png);
-                background: #161a21;
-                border: none;
-                padding-right: 3px;
-            }
-
-
-        """
-        self.css_maximize ="""
-            QToolButton[accessibleName="btn_max"] {
-                image: url(./icons/nav_maximize.png);
-                background: #161a21;
-                border: nobutton_stylene;
-                padding-right: 3px; 
-            }
-            QToolButton[accessibleName="btn_max"]:hover {
-                image: url(./icons/colored_maximize.png);
-                background: #161a21;
-                border: none;
-            }
-        
-        """
-        self.css_collapse ="""
-            QToolButton[accessibleName="btn_max"]{
-                image: url(./icons/nav_normal.png);
-                background: #161a21;
-                border: none;
-                
-            }
-            QToolButton[accessibleName="btn_max"]:hover{
-                image: url(./icons/colored_normal.png);
-                background: #161a21;
-                border: none;
-                
-            }
-        """
-        self.current_editor = self.create_editor()
-        self.current_editor.setFocus()
-        self.text_editors = []
-        self.tabs = qtw.QTabWidget()
-        self.tabs.setTabsClosable(True)
-        self.tabs.tabBar().setMovable(True)
-
+ 
         self._createActions()
         self._connectActions()
         
         self.l = qtw.QHBoxLayout()
         self.l.setContentsMargins(0,0,10,0)
         self.menubar = qtw.QMenuBar()
-        self.menubar.setStyleSheet(css)  
+    
         
         file_menu = self.menubar.addMenu('File')
         file_menu.addAction(self.new_action)
@@ -116,25 +42,40 @@ class TitleBar(qtw.QWidget):
         self.window_title.setAccessibleName("lbl_title") 
         self.window_title.setFixedHeight(self.height)
         self.l.addWidget(self.window_title)
-        self.window_title.setStyleSheet(css)
+
 
         self.setSizePolicy(qtw.QSizePolicy.Expanding, qtw.QSizePolicy.Fixed)
         self.maxNormal=False
        
         self.closeButton = qtw.QToolButton() 
         self.closeButton.setAccessibleName("btn_close")                           
-        self.closeButton.setStyleSheet(css)
+
         self.closeButton.clicked.connect(self.on_click_close)
 
         self.maxButton = qtw.QToolButton()
         self.maxButton.setAccessibleName("btn_max")  
-        self.maxButton.setStyleSheet(self.css_maximize)
+        self.maxButton.setStyleSheet(
+            """
+            QToolButton[accessibleName="btn_max"] {
+                image: url(./icons/nav_maximize.png);
+                background: #161a21;
+                border: nobutton_stylene;
+                padding-right: 3px; 
+            }
+            QToolButton[accessibleName="btn_max"]:hover {
+                image: url(./icons/colored_maximize.png);
+                background: #161a21;
+                border: none;
+            }
+        
+        """
+        )
         self.maxButton.clicked.connect(self.showMaxRestore)
 
         self.hideButton = qtw.QToolButton()
         self.hideButton.setAccessibleName("btn_min")  
         self.hideButton.clicked.connect(self.on_click_hide)
-        self.hideButton.setStyleSheet(css)
+
 
         self.l.addWidget(self.hideButton)
         self.l.addWidget(self.maxButton)
@@ -164,104 +105,58 @@ class TitleBar(qtw.QWidget):
 
     def _connectActions(self):
         # Connect File actions
-        self.new_action.triggered.connect(self.new_tab)
-        self.open_action.triggered.connect(self.open_document)
-        self.save_action.triggered.connect(self.save_document)
+        # self.new_action.triggered.connect(self.new_tab)
+        # self.open_action.triggered.connect(self.open_document)
+        # self.save_action.triggered.connect(self.save_document)
         self.exit_action.triggered.connect(self.close)
 
     def close(self):
         qtw.QApplication.quit()
-
-    def new_tab(self, checked = False, title = "Untitled.txt"):
-        w = qtw.QMainWindow()
-        self.tabs.addTab(w, title)
-        self.tabs.setCurrentWidget(self.current_editor) # set the current tab selected as current widget
         
-        # create toolbars
-        file_toolbar = w.addToolBar("File")
-        file_toolbar.setIconSize(qtc.QSize(22,22))
-        # file_toolbar.setMovable(False)
-        file_toolbar.addAction(self.new_action)
-        file_toolbar.addAction(self.open_action)
-        file_toolbar.addAction(self.save_action)
-        
-        self.current_editor = self.create_editor() # create a QTextEdit
-        self.text_editors.append(self.current_editor) # add current editor to the array list 
-       
-        w.setCentralWidget(self.current_editor)
-
-    def create_editor(self):
-        current_editor = qtw.QTextEdit()
-        # Set the tab stop width to around 33 pixels which is
-        # about 8 spaces
-        current_editor.setTabStopWidth(33)
-        return current_editor
-
-    def change_text_editor(self, index):
-        if index < len(self.text_editors):
-            self.current_editor = self.text_editors[index]
-
-    def remove_editor(self, index):
-        if self.tabs.count() < 2:
-            return
-        
-        self.tabs.removeTab(index)
-        if index < len(self.text_editors):
-            del self.text_editors[index]
-
-    def open_document(self):
-        options = qtw.QFileDialog.Options()
-        # Get filename and show only .notes files
-        #PYQT5 Returns a tuple in PyQt5, we only need the following filenames
-        self.filename, _ = qtw.QFileDialog.getOpenFileName(
-            self, 'Open File',".",
-            "(*.notes);;Text Files (*.txt);;Python Files (*.py)",
-            options=options
-        )
-        if self.filename:
-            with open(self.filename,"rt") as file:
-                content = file.read()
-                self.current_editor = self.create_editor() 
-                currentIndex = self.tabs.addTab(self.current_editor, str(self.filename))   # use that widget as the new tab
-                self.current_editor.setText(content) # set the contents of the file as the text
-                self.tabs.setCurrentIndex(currentIndex) # make current opened tab be on focus
-
-    def save_document (self):
-        if not self.current_editor.document().isModified():
-            self.statusBar().showMessage("There are no texts to be saved!")
-        else:
-            # Only open dialog if there is no filename yet
-            #PYQT5 Returns a tuple in PyQt5, we only need the filename
-            options = qtw.QFileDialog.Options()
-            file_filter = 'Notes_ file (*.notes);; Text file (*.txt);; Python file (*.py)'
-            if not self.filename:
-                self.filename = qtw.QFileDialog.getSaveFileName(self,caption='Save File',directory=".",filter=file_filter,initialFilter='Notes Files (*.notes)')[0] # zero index is required, otherwise it would throw an error if no selection was made
-            
-            if self.filename:
-
-                # We just store the contents of the text file along with the
-                # format in html, which Qt does in a very nice way for us
-                with open(self.filename,"wt") as file:
-                    file.write(self.current_editor.toHtml())
-                    print(self.tabs.currentIndex())
-                    print(str(self.filename))
-                    self.tabs.setTabText(self.tabs.currentIndex(), str(self.filename)) # renames the current tabs with the filename
-                    self.statusBar().showMessage(f"Saved to {self.filename}")
-                    
-                self.changesSaved = True
-    
+    #####################################################
+    ## TITLE BAR MINIMIZE, MAXIMIZE, CLOSE METHODS
+    #####################################################
     def showMaxRestore(self):
         if(self.maxNormal):
             main.showNormal()
             self.maxNormal= False
             print('nomalscreen: maximize icon showing')
-            self.maxButton.setStyleSheet(self.css_maximize)
+            self.maxButton.setStyleSheet(
+            """
+                QToolButton[accessibleName="btn_max"] {
+                    image: url(./icons/nav_maximize.png);
+                    background: #161a21;
+                    border: nobutton_stylene;
+                    padding-right: 3px; 
+                }
+                QToolButton[accessibleName="btn_max"]:hover {
+                    image: url(./icons/colored_maximize.png);
+                    background: #161a21;
+                    border: none;
+                }
+            """
+            )
             
         else:
             main.showMaximized()
             self.maxNormal=  True
             print('fullscreen: collapse icon showing')
-            self.maxButton.setStyleSheet(self.css_collapse)
+            self.maxButton.setStyleSheet(
+            """
+                QToolButton[accessibleName="btn_max"]{
+                    image: url(./icons/nav_normal.png);
+                    background: #161a21;
+                    border: none;
+                    
+                }
+                QToolButton[accessibleName="btn_max"]:hover{
+                    image: url(./icons/colored_normal.png);
+                    background: #161a21;
+                    border: none;
+                    
+                }
+            """
+            )
 
     def on_click_maximize(self):
         self.maximaze = not self.maximaze
@@ -293,7 +188,7 @@ class TitleBar(qtw.QWidget):
         # return super(MainWindow, self).eventFilter(obj, event)
 
     def on_click_close(self):
-        main.close()
+        qtw.QApplication.quit()
             
     def on_click_hide(self):
         main.showMinimized()
@@ -321,17 +216,15 @@ class TitleBar(qtw.QWidget):
         if event.type() == event.WindowStateChange:
             self.titleBar.windowStateChanged(self.windowState())
 
-
+    #####################################################
+    ##                      END
+    #####################################################
 
 class MainWindow(qtw.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.statusbar = self.statusBar()
         self.statusbar.showMessage("Ready")
-
-        self.current_editor = self.create_editor()
-        self.current_editor.setFocus()
-        self.text_editors = []
 
         # WINDOW FLAGS: https://doc.qt.io/qtforpython/overviews/qtwidgets-widgets-windowflags-example.html?highlight=windowminimizebuttonhint
         self.setMinimumSize(400,250)
@@ -342,9 +235,19 @@ class MainWindow(qtw.QMainWindow):
                             | qtc.Qt.WindowMaximizeButtonHint
                             | qtc.Qt.WindowCloseButtonHint)
 
+        self.filename = ""
+        self.changesSaved = False
+        self.current_editor = self.create_editor()
+        self.current_editor.setFocus()
+        self.text_editors = []
+
         self.title_bar  = TitleBar(self) 
         self.tabs = qtw.QTabWidget()
         self.tabs.setTabsClosable(True)
+        self.tabs.setDocumentMode(True) # let's you double click tab bar to create new tabs
+        self.tabs.tabBarDoubleClicked.connect(self.tab_open_doubleclick)
+        self.tabs.tabCloseRequested.connect(self.remove_editor)
+        self.tabs.currentChanged.connect(self.change_text_editor)
         self.tabs.tabBar().setMovable(True)
         
         self.getActions = TitleBar(self)
@@ -362,6 +265,26 @@ class MainWindow(qtw.QMainWindow):
         widget.setLayout(l)
         self.setCentralWidget(widget)
         self.new_tab()
+
+    def _connectActions(self):
+        # Connect File actions
+        self.new_action.triggered.connect(self.new_tab)
+        self.exit_action.triggered.connect(self.close)
+
+    def tab_open_doubleclick(self, index):
+        if index == -1:
+            self.new_tab()
+
+    def remove_editor(self, index):
+        if self.tabs.count() < 2:
+            return
+        self.tabs.removeTab(index)
+        if index < len(self.text_editors):
+            del self.text_editors[index]
+
+    def change_text_editor(self, index):
+        if index < len(self.text_editors):
+            self.current_editor = self.text_editors[index]
 
     def new_tab(self, checked = False, title = "Untitled.txt"):
         w = qtw.QMainWindow()
@@ -388,17 +311,7 @@ class MainWindow(qtw.QMainWindow):
         current_editor.setTabStopWidth(33)
         return current_editor
 
-    def change_text_editor(self, index):
-        if index < len(self.text_editors):
-            self.current_editor = self.text_editors[index]
 
-    def remove_editor(self, index):
-        if self.tabs.count() < 2:
-            return
-        
-        self.tabs.removeTab(index)
-        if index < len(self.text_editors):
-            del self.text_editors[index]
 
 
 if __name__ == "__main__":
@@ -486,7 +399,70 @@ if __name__ == "__main__":
                 padding: 5px 10px 5px 10px;
             }
             
+            QToolButton[accessibleName="btn_max"]{
+                image: url(./icons/nav_normal.png);
+                background: #161a21;
+                border: none;
+                
+            }
+            QToolButton[accessibleName="btn_max"]:hover{
+                image: url(./icons/colored_normal.png);
+                background: #161a21;
+                border: none;
+                
+            }
+
+            QToolButton[accessibleName="btn_max"] {
+                image: url(./icons/nav_maximize.png);
+                background: #161a21;
+                border: nobutton_stylene;
+                padding-right: 3px; 
+            }
+            QToolButton[accessibleName="btn_max"]:hover {
+                image: url(./icons/colored_maximize.png);
+                background: #161a21;
+                border: none;
+            }
+
+            QMenuBar{
+                color: #fff;
+                font: "Consolas";
+                font-size: 14px;
+                padding: 3px; 
+            }
+           
+            QLabel[accessibleName="lbl_title"]{
+                background-color: #161a21; 
+                font-size: 13px;
+                font: "Consolas";
+                padding-right: 425px;
+            }
+            QToolButton[accessibleName="btn_close"] {
+                image: url(./icons/nav_close.png);
+                background: #161a21;
+                border: none;
+               
+            }
+            QToolButton[accessibleName="btn_close"]:hover {
+                image: url(./icons/colored_close.png);
+                background: #161a21;
+                border: none;
+            }    
+            QToolButton[accessibleName="btn_min"] {
+                image: url(./icons/nav_minimize.png);
+                background: #161a21;
+                border: none;
+                padding-right: 3px;
+            }
+            QToolButton[accessibleName="btn_min"]:hover {
+                image: url(./icons/colored_minimize.png);
+                background: #161a21;
+                border: none;
+                padding-right: 3px;
+            }
+
         """
+
 
     )
     main.show()
