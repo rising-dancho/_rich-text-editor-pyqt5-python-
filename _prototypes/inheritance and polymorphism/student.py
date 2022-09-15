a06f331d05414f834290a6dc43c7f1f5c584afb3
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-09-15 18:13:03 +0800

    understanding fundamentals of OOP

--- _prototypes/inheritance and polymorphism/student.py
+++ _prototypes/inheritance and polymorphism/student.py
@@ -1,20 +1,26 @@
 # FUNDAMENTALS OF OBJECT ORIENTED PROGRAMMING: 
 # https://www.youtube.com/watch?v=e4fwY9ZsxPw&t=3599s
 
-class Student:
-    def __init__(self, name, house):
-        self.name = name
-        self.house = house
-        
-def main():
-    first, last, house = get_student()
-    print(f"{first}, {last} from {house}")
+
+# this Student "class" is kinda like the "soul" of the Student object
+class Student: 
+    def __init__(self, name, house): # this allows to pass in parameters into the declaration object
+        self.name = name # see method "get_student"
+        self.house = house # we passed "name" and "house" parameters to the Student class
 
 def get_student():
-    first = input("First Name: ")
-    last = input("Last Name: ")
+    name = input("Name: ") # passing this inputs from the user as an arguments
     house = input("House: ")
-    return first, last, house
+    
+    student = Student(name, house) # declaration of the object. or the "body" for my "soul".
+
+    return student # see main method where the "get_student" object is declared
+
+def main():
+    student = get_student() # this object declaration takes the return statement from the "get_student" method
+    print(f"{student.name} from {student.house}")
+
+
 
 if __name__ == "__main__":
     main()
