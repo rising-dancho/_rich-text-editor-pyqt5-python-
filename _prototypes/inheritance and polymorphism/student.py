7286cefa073940045345cbb9d4028f2a94f9e7da
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-09-17 05:34:58 +0800

    learning fundamentals of OOP

--- _prototypes/inheritance and polymorphism/student.py
+++ _prototypes/inheritance and polymorphism/student.py
@@ -1,28 +1,34 @@
 # FUNDAMENTALS OF OBJECT ORIENTED PROGRAMMING: 
 # https://www.youtube.com/watch?v=e4fwY9ZsxPw&t=3599s
 
+# ABOUT DEFUALT ARGUMENTS: https://bobbyhadz.com/blog/python-syntaxerror-non-default-argument-follows-default-argument#:~:text=The%20Python%20%22SyntaxError%3A%20non%2D,positional%20parameters%20of%20the%20function.
+
 
 # this Student "class" is kinda like the "soul" of the Student object
 class Student: # you can make parameters opational by attaching "None" as in house=None
-    def __init__(self, name, house=None): # this allows to pass in parameters into the declaration object
+    def __init__(self, name, spell, house=None, ): # this allows to pass in parameters into the "declaration object"
         # Exception Handling codes below
         if name == "":
            raise ValueError("Missing Name")
         
-        if house not in ["Gryff","Slyth", "Huff","Raven"]: # not in allows for an array of values
+        if house not in ["gryff","slyth", "huff","raven"]: # not in allows for an array of values
            raise ValueError("Invalid House")
         
-        self.name = name # see method "get_student"
+        # notice that it doesn't have to be in order. the setters does not have to follow the order in which the parameters are declared
+        # also, positional arguments (self, name, spell) should be declared AFTER the default arguments
+        self.name = name # see methodtae "get_student"
         self.house = house # we passed "name" and "house" parameters to the Student class
+        self.patronus = spell # just added aa 4rth parameter. nothing special
 
     # special python method in printing objects on the console
     def __str__(self): # this prevents this error: object at 0x000002CD04FEBEE0
-        return f"{self.name} from {self.house}"
+        return f"{self.name} from {self.house} and can use the spell {self.patronus}"
 
 def get_student():
     name = input("Name: ") # passing this inputs from the user as an arguments
     house = input("House: ")
-    student = Student(name, house) # declaration of the object. or the "body" for my "soul".
+    spell = input("Spell: ")
+    student = Student(name, house, spell) # declaration of the object. or the "body" for my "soul".
     
     return student # see main method where the "get_student" object is declared
 
@@ -30,7 +36,7 @@ def main():
     student = get_student() # this object declaration takes the return statement from the "get_student" method
     # print(f"{student.name} from {student.house}")
     
-    print(student)
+    print(student) # print triggers the __str__ method, because it's looking for a string
 
 
 if __name__ == "__main__":
