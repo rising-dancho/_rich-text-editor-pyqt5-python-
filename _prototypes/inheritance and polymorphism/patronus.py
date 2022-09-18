efef2b26dcb2c9328cd5b5868d6b0b17abef8393
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-09-18 10:22:09 +0800

    learning the fundamentals of OOP

--- _prototypes/inheritance and polymorphism/patronus.py
+++ _prototypes/inheritance and polymorphism/patronus.py
@@ -6,7 +6,7 @@
 
 # this Student "class" is kinda like the "soul" of the Student object
 class Student: # you can make parameters opational by attaching "None" to a parameter, as in house=None
-    def __init__(self, name, house=None): # this allows to pass in parameters into the "declaration object"
+    def __init__(self, name, house=None, patronus='Unknown'): # this allows to pass in parameters into the "declaration object"
         # Exception Handling codes below
         if name == "":
            raise ValueError("Missing Name")
@@ -19,12 +19,24 @@ class Student: # you can make parameters opational by attaching "None" to a para
         # also, positional arguments (self, name) should be declared AFTER the default arguments (house, and spell)
         self.name = name # see methodtae "get_student"
         self.house = house # we passed "name" and "house" parameters to the Student class
-    
+        self.patronus = patronus # just added aa 4rth parameter. nothing special
 
     # special python method in printing objects on the console
     def __str__(self): # this prevents this error: object at 0x000002CD04FEBEE0
-        return f"Behold! {self.name} from house {self.house}! Let it be known!!"
+        return f"Behold! {self.name} from {self.house} has the patronus {self.patronus}! Let it be known!!"
 
+    def charm(self): # "self" is a positional argument that is required for methods that are declared INSIDE a class
+        # this "self" argument is allowing access to a method owned by the Student class if in case you are trying to call this function OUTSIDE of the Student class
+        # below is the syntax of python for a "switch statement" (if you do java)    
+        match self.patronus: 
+            case "Stag":
+                return "🦌"
+            case "Otter":
+                return "🦦"
+            case "Jack Russel Terrier":
+                return "🐕"
+            case _: # this is returned if the user did not input anything for the "Patronus:"
+                return "💫"
 
 def greet(name, message='Hi'): # we can call this method just by declaring it in our main method and providing the parameters that it is asking for
     return f"{message}, {name}!"
@@ -32,18 +44,19 @@ def greet(name, message='Hi'): # we can call this method just by declaring it in
 
 def get_student():
     name = input("\n\nName: ") # passing this inputs from the user as an arguments
+    
     house = input("House: ")
-    student = Student(name, house) # declaration of the object. or the "body" for my "soul".
+    patronus = input("Patronus: ")
+    student = Student(name, house, patronus) # declaration of the object. or the "body" for my "soul".
     
     return student # see main method where the "get_student" object is declared
 
 def main():
     student = get_student() # this object declaration takes the return statement from the "get_student" method
 
-    print(student) # calling the charm method of the "Student" class
-    # print will trigger the __str__ method located inside the Student class, because the "print" method is looking for a "string" input
+    print("\n\nExpecto Patronum!", student.charm()) # calling the charm method of the "Student" class
+    print(student,"\n") # print will trigger the __str__ method located inside the Student class, because the "print" method is looking for a "string" input
     # notice that inside the "get_student" method the Student class is referenced and given variables (name, house, patronus) and then returned student
-
 
     greeting = greet(student.name)
     print(greeting, "<3 \n")
