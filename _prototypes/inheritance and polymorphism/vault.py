ae3490de1be6208aeeb49535194dc7ce6a31b7e8
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-09-20 14:17:55 +0800

    learning the fundamentals of OOP

--- _prototypes/inheritance and polymorphism/vault.py
+++ _prototypes/inheritance and polymorphism/vault.py
@@ -1,4 +1,5 @@
 # SOURCE: https://www.youtube.com/watch?v=e4fwY9ZsxPw&list=PLPG6wUjL9QmTKKoO5lrI-l0Tt2bCMRgS2&index=14
+# OPERATOR OVERLOADING: https://www.programiz.com/python-programming/operator-overloading
 
 class Name:
     def __init__(self, name): 
@@ -17,15 +18,24 @@ class Vault(Name):
     def __str__(self):
         return f"{self.name} Galleons: {self.galleons}, Sickles: {self.sickles}, Knuts: {self.knuts}"
 
+    # OPERATOR OVERLOADING
+    def __add__(self, other):
+        galleons = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        knuts = self.knuts + other.knuts
+        return Vault("", galleons, sickles, knuts)
+
 potter = Vault("Potter: ",100,20,50)
 potter.inventory()
 
 weasly = Vault("Weasly: ",20,50,6)
 print(weasly)
 
-galleons = potter.galleons + weasly.galleons
-sickles = potter.sickles + weasly.sickles
-knuts = potter.knuts + weasly.knuts
+# galleons = potter.galleons + weasly.galleons
+# sickles = potter.sickles + weasly.sickles
+# knuts = potter.knuts + weasly.knuts
 
-total = galleons + sickles + knuts
+# total = galleons + sickles + knuts
+
+total = potter + weasly
 print("Total: ", total)
