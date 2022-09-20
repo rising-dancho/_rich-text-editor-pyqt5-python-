177cd8d2b73f3b04d252f53f69601a9a1e674cd5
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-09-20 14:58:53 +0800

    inheritance and polymorphism combined

--- _prototypes/inheritance and polymorphism/polymorphism.py
+++ _prototypes/inheritance and polymorphism/polymorphism.py
@@ -1,4 +1,5 @@
 # CLASSES POLYMORPHISM: https://www.programiz.com/python-programming/polymorphism
+# PYTHON INHERITANCE: https://www.programiz.com/python-programming/inheritance
 
 # What is Polymorphism?
 # -- The literal meaning of polymorphism is the condition of occurrence in different forms.
@@ -6,10 +7,20 @@
 # -- It refers to the use of a single type entity (method, operator or object) to represent 
 # -- different types in different scenarios.
 
-class Cat:
+# What is Inheritance?
+# -- Inheritance enables us to define a class that takes all the functionality 
+# -- from a parent class and allows us to add more. 
+
+
+# INHERITANCE
+class About:
     def __init__(self, name, age):
         self.name = name
-        self.age = age
+        self.age  = age
+
+class Cat(About): # inherits the functionality of the parent class About
+    def __init__(self, name, age):
+        super().__init__(name, age) # super refers to the parent. we are calling the parent's __init__ function
 
     def info(self):
         print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
@@ -18,10 +29,9 @@ class Cat:
         print("Meow")
 
 
-class Dog:
+class Dog(About): # inherits the functionality of the parent class About
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        super().__init__(name, age) # super refers to the parent. we are calling the parent's __init__ function
 
     def info(self):
         print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
