a6fdea7ff2c2ddd6983ef1b56cde12e64505a345
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-09-22 11:54:59 +0800

    creating prototype

--- _prototypes/working_on/working_on.py
+++ _prototypes/working_on/working_on.py
@@ -6,7 +6,7 @@ from PyQt5 import QtGui as qtg
 
 class TitleBar(qtw.QWidget):
     height = 35
-    def __init__(self, parent=None):
+    def __init__(self, parent):
         super(TitleBar, self).__init__(parent)
 
         self.current_editor = self.parent().create_editor()
